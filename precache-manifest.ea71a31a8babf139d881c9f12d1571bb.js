1a5b0bc0e4ee5450369e60f531fa4aaf05ed2282
Oleksandr Dievri <o.dievri@outlook.com>
2019-02-18 18:33:19 +0200

    initial commit

---
+++ precache-manifest.ea71a31a8babf139d881c9f12d1571bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3adb6f66f1be1ccc93a6",
+    "url": "/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "3adb6f66f1be1ccc93a6",
+    "url": "/static/js/main.3adb6f66.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "7f23a7082bf990e7afd4b364dc759ae8",
+    "url": "/index.html"
+  }
+];

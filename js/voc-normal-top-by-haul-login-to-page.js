5a5b81abd4cba6f39c2882776c6bbf5982927655
dievri <dievri@users.noreply.github.com>
2021-01-23 01:32:24 +0000

    deploy: kghackers/kgparser@6d7c8db815d7289475eabed94d8898b6da7cba91

--- js/voc-normal-top-by-haul-login-to-page.js
+++ js/voc-normal-top-by-haul-login-to-page.js
@@ -2,4 +2,4 @@ if (typeof STATS_DATA === 'undefined') {
     var STATS_DATA = {};
 }
 
-STATS_DATA.topByHaulLoginToPage = {"jurist":1,"????????":1,"mephisto":1,"saida":1,"sallynar":1};
+STATS_DATA.topByHaulLoginToPage = {"????????":1,"saida":1};
